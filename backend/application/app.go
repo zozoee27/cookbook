b823f0f2f3c9bc08d9cb16679d44922fb9a43161
Zoey Lan <zoey@Zoeys-MacBook-Air.local>
2021-02-15 14:51:00 -0700

    Set up server to use HTTPS instead of HTTP

--- backend/application/app.go
+++ backend/application/app.go
@@ -29,7 +29,11 @@ func (a *App) Initialize(databaseName string) {
 
 func (a *App) Run(address string) {
 	log.Print("Cookbook server is running and listening on port: ", address)
-	http.ListenAndServe(address, a.Router)
+	err := http.ListenAndServeTLS(address, "/etc/ssl/localhost-certs/localhost.crt", "/etc/ssl/localhost-certs/localhost.key", a.Router)
+
+	if err != nil {
+		log.Fatal("Could not start server: ", err)
+	}
 }
 
 func (a *App) StopApplication() {
@@ -39,5 +43,9 @@ func (a *App) StopApplication() {
 func (a *App) initializeRoutes() {
 	log.Print("Initializing routes")
 	a.Router.HandleFunc("/account/register", a.AccountManager.RegisterAccount).Methods("POST")
+	a.Router.HandleFunc("/", home)
+}
 
+func home(w http.ResponseWriter, r *http.Request) {
+	log.Print("Recieved request for /")
 }
